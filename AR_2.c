9aa1a824b4b6a3e56f6eb81b0b54a95bc43c9c34
Ayushman Raj <AYUSHMAN7206@GMAIL.COM>
2026-08-17 18:33:43 +0530

    Program to print my personal details

---
+++ AR_2.c
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    char name[100]="Ayushman Raj";
+    char Fathers_name[100]="Manoj Kumar Sharma";
+    char Mothers_name[100]="Kusum Sharma";
+    char city_name[100]="Varanasi";
+    int age=18;
+    long int Phone_no=8176066278;
+    printf("name:%s\n",name);
+    printf("Fathers_name:%s\n",Fathers_name);
+    printf("Mothers_name:%s\n",Mothers_name);
+    printf("city_name:%s\n",city_name);
+    printf("age:%d\n",age);
+    printf("Phone_no:%s\n",Phone_no);
+
+    return 0;
+}
