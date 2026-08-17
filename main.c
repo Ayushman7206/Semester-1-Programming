5012024b00fae37edb113aaac8d7f5c1a5c1f486
Ayushman Raj <AYUSHMAN7206@GMAIL.COM>
2026-08-17 18:26:47 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    char name[100]="Ayushman Raj";
+    char collage[100]="UIT";
+    char branch[100]="AIML";
+    int age=18;
+    int roll_no=20;
+    printf("Name:%s\n",name);
+    printf("collage:%s\n",collage);
+    printf("branch:%s\n",branch);
+    printf("age:%d\n",age);
+    printf("Roll_no:%d\n",roll_no);
+    
+    return 0;
+}
