0fbd72bda1e85580765c84fe9d6f0dc977fc146e
Ayushman Raj <AYUSHMAN7206@GMAIL.COM>
2026-08-20 17:18:56 +0530

    program to perform bitwise operations

---
+++ Bitwise_operations.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{   int a=3, b=2;
+    printf("a&b:%d\n",a&b);
+    printf("a|b:%d\n",a|b);
+    printf("a^b:%d\n",a^b);
+    printf("~a:%d\n",~a);
+    printf("~b:%d\n",~b);
+    printf("a<<b:%d\n",a<<b);
+    printf("a>>b:%d\n",a>>b);
+
+    return 0;
+}
